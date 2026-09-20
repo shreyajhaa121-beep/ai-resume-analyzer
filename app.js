083f7bd32609ae5fc36b022d96cc2ab75346bccf
Shreya Kumari <shreyajhaa121@gmail.com>
2026-09-20 17:45:10 +0530

    Remove ATS category debug code

--- app.js
+++ app.js
@@ -903,17 +903,7 @@ function analyzeResume() {
 
 const categoryScores =
     calculateCategoryScores(resumeText);
-    
-    console.log("CATEGORY SCORES:", categoryScores);
-    
-alert(
-    "Programming: " + categoryScores.programming + "%\n" +
-    "Web: " + categoryScores.web + "%\n" +
-    "Database: " + categoryScores.database + "%\n" +
-    "Tools: " + categoryScores.tools + "%\n" +
-    "Concepts: " + categoryScores.concepts + "%\n" +
-    "Soft Skills: " + categoryScores.softSkills + "%"
-);
+
 console.log(
     "ATS Category Scores:",
     categoryScores
